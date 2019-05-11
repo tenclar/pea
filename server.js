87c722bc02925b4591e00204fb6f6796b04f6cc7
Tenclar Valus <tenclarvalus@gmail.com>
2019-05-10 19:44:37 -0500

    configuração servidor

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded( { extended:false } ))
 const Usuarios = require('./routes/Usuarios')
 app.use('/usuarios', Usuarios)
 
+const Estados = require('./routes/Estados')
+app.use('/estados', Estados)
+
 app.listen(port, () => {
     console.log("Server is running on port: " + port)
 })
